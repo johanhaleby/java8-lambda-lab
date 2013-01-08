5f5d6b5ef7b3ed932db11ce45aaf987f88c0c23f
Johan Haleby <johan.haleby@gmail.com>
2013-01-08 20:15:21 +0100

    Initial commit

---
+++ src/main/java/com/jayway/lab/WordCounter.java
@@ -0,0 +1,7 @@
+package com.jayway.lab;
+
+public class WordCounter {
+
+
+
+}
---
+++ src/test/java/com/jayway/lab/WordCounterSpec.java
@@ -0,0 +1,20 @@
+package com.jayway.lab;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for Word Counter.
+ */
+public class WordCounterSpec {
+
+    @Test public void
+    _() {
+        Assert.fail("ikk");
+        // Given
+        // When
+        // Then
+    }
+
+
+}
